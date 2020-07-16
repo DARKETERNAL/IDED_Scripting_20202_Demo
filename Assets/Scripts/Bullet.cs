56b50398e963a0ea52eecd52b38b7a644dc1f5b0
Andres Perez Campillo <tomailsandstuff@gmail.com>
2020-07-16 08:58:15 -0500

    Added autodestroy to Bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float autoDestroyTime = 1.5F;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Destroy(gameObject, autoDestroyTime);
+    }
+}
